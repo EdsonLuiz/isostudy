4d4ce7ac4ce64ded612f41d87b5dd3194d33679b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-01-04 11:16:20 -0200

    Add card group

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,12 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
+import Card from '../components/Card'
+
+import wallpaper from '../images/wallpaper.jpg'
+import wallpaper2 from '../images/wallpaper2.jpg'
+import wallpaper3 from '../images/wallpaper3.jpg'
+import wallpaper4 from '../images/wallpaper4.jpg'
 
 const IndexPage = () => (
   <Layout>
@@ -11,11 +17,41 @@ const IndexPage = () => (
     <div className="hero">
       <div className="hero-group">
         <h1>Learn to design and code React apps</h1>
-        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
         <Link to="/page-2/">Watch the video</Link>
       </div>
     </div>
-{/*     
+
+    <div className="cards">
+      <h2>11 Courses, more coming</h2>
+      <div className="card-group">
+        <Card
+          title="Design System with Figma"
+          text="10 sections"
+          image={wallpaper}
+        />
+        <Card
+          title="React for designers"
+          text="12 sections"
+          image={wallpaper2}
+        />
+        <Card
+          title="Video Editing with Screenflow"
+          text="6 sections"
+          image={wallpaper3}
+        />
+        <Card
+          title="Sound Design with Cubase"
+          text="6 sections"
+          image={wallpaper4}
+        />
+      </div>{' '}
+      {/* END .card-group */}
+    </div>
+    {/*     
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
