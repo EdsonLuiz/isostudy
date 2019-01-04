8c644ee0ea8bc87c1b9de6f98febb577feca4164
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-01-04 14:10:17 -0200

    Add css rule on header based on scroll event

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,49 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 import './Header.css'
 
 import logo from '../images/logo-designcode.svg'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-     <div className="header-group">
-       <Link to="/">
-        <img src={logo} alt="" width="30"/>
-       </Link>
-       <Link to="/courses">Courses</Link>
-       <Link to="/downloads">Downloads</Link>
-       <Link to="/workshops">Workshops</Link>
-       <Link to="/buy"><button>Buy</button></Link>
-     </div>
-  </div>
-)
+class Header extends Component {
+  state = {
+    hasScrolled: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? 'header header-scrolled' : 'header'}
+      >
+        <div className="header-group">
+          <Link to="/">
+            <img src={logo} alt="" width="30" />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
